b64cb889b5313d03766481fcac1ecb00879a0c46
David Sisalema <dsisalema@gestor.co>
2022-03-15 20:59:39 -0500

    Login y Auth

---
+++ src/app/interfaces/register-form.interface.ts
@@ -0,0 +1,7 @@
+export interface RegisterForm {
+    nombre: String;
+    email: String;
+    password: String;
+    password2: String;
+    terminos  : boolean;
+}
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,5 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AuthGuard } from '../guards/auth.guard';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -9,10 +11,12 @@ import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 
 
+
 const routes: Routes = [
 
     { path: 'dashboard', 
     component: PagesComponent,
+      canActivate: [ AuthGuard ],
     children: [
       { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard'} },
       { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress'} },
